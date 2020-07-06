cc4719913521b432ebd90a2d25fbf27fb479ef7a
Dima Klymiuk <dimon4egkl55@gmail.com>
2020-07-06 12:47:36 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+
+ #TOKEN = "1000861809:AAEKB-wTfTno1vE6HauzXVVcJn-QkUjXdYU"   old token
+TOKEN = "1363725527:AAEUMqfYyo9R56Y2rbB8Xu7tE9OygbSyEhI" # token for eu_life
+
+CHAT_ID = -415621154 # real group
+GROUP_CHAT_ID = -327342420 # for text
